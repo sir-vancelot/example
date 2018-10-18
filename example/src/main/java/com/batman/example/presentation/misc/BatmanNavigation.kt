9ef91bf9c09e0dad0c99a51d9f0e462f9ac90940
Vance Ehrlich <vance.ehrlich@gmail.com>
2018-10-18 16:47:35 -0400

    Add a missing comment

--- example/src/main/java/com/batman/example/presentation/misc/BatmanNavigation.kt
+++ example/src/main/java/com/batman/example/presentation/misc/BatmanNavigation.kt
@@ -1,5 +1,7 @@
 package com.batman.example.presentation.misc
 
 open class BatmanNavigation {
+    // <editor-fold desc="Navigate to the view corresponding to the provided tag if the navigation can">
     open fun navigateTo(tag: String) { }
+    // </editor-fold>
 }
