f79f54e67b9d275ccad8eb661dc2db0747527035
Vance Ehrlich <vance.ehrlich@gmail.com>
2018-10-22 14:31:31 -0400

    Interface model mappers

--- example/src/main/java/com/batman/example/infrastructure/mapper/ExampleStringModelMapper.kt
+++ example/src/main/java/com/batman/example/infrastructure/mapper/ExampleStringModelMapper.kt
@@ -5,9 +5,9 @@ import com.batman.example.infrastructure.db.DBHelper
 import com.batman.example.infrastructure.db.query.ExampleStringQuery.COLUMNS
 import com.batman.example.domain.model.ExampleStringModel
 
-object ExampleStringModelMapper {
+object ExampleStringModelMapper: ModelMapper<ExampleStringModel> {
     // <editor-fold desc="Transform all of the entries in a cursor to their corresponding model">
-    fun transformList(cursor: Cursor): ArrayList<ExampleStringModel> {
+    override fun transformList(cursor: Cursor): ArrayList<ExampleStringModel> {
         val exampleStrings = ArrayList<ExampleStringModel>()
         while(cursor.moveToNext()) {
             exampleStrings.add(transform(cursor))
@@ -17,7 +17,7 @@ object ExampleStringModelMapper {
     // </editor-fold>
 
     // <editor-fold desc="Transform the entry currently pointed to its corresponding model">
-    private fun transform(cursor: Cursor): ExampleStringModel {
+    override fun transform(cursor: Cursor): ExampleStringModel {
         val exampleId = DBHelper.getString(cursor, COLUMNS.EXAMPLE_ID)
         val exampleString = DBHelper.getString(cursor, COLUMNS.EXAMPLE_STRING)
 
---
+++ example/src/main/java/com/batman/example/infrastructure/mapper/ModelMapper.kt
@@ -0,0 +1,13 @@
+package com.batman.example.infrastructure.mapper
+
+import android.database.Cursor
+
+interface ModelMapper<Model> {
+    // <editor-fold desc="Transform all of the entries in a cursor to their corresponding model">
+    fun transformList(cursor: Cursor): List<Model>
+    // </editor-fold>
+
+    // <editor-fold desc="Transform the entry currently pointed to its corresponding model">
+    fun transform(cursor: Cursor): Model
+    // </editor-fold>
+}
