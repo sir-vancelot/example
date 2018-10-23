4ea65e97004a33ee6f4039cc9100c21d51eda7fe
Vance Ehrlich <vance.ehrlich@gmail.com>
2018-10-23 18:20:36 -0400

    Update a comment on the application module

--- example/src/main/java/com/batman/example/di/ApplicationModule.kt
+++ example/src/main/java/com/batman/example/di/ApplicationModule.kt
@@ -9,7 +9,7 @@ import javax.inject.Singleton
 @Module
 class ApplicationModule(private val application: ExampleApplication) {
     /*
-       Classes that should be readily available while the modules corresponding activity is alive
+       Classes that should be readily available while the modules corresponding application is alive
        should be provided with the @Singleton scope. Classes that do not hold data like commandHandlers
        and queryHandlers should not be. Instead, they should be provided with @Reusable which marks
        them for garbage collection.
